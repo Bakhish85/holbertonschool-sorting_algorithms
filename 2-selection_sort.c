6e0304ce412dac1fcf2dfde8a4980ca4d1baabd8
Bakhish85 <umudlu85@gmail.com>
2024-04-17 08:55:11 +0000

    swap

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,18 @@
 #include "sort.h"
 /**
+ * _swap - swap two numbers
+ * @a: integer
+ * @b: integer
+ */
+void _swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+/**
  * selection_sort - sort array using selection sort algorithm
  * @array: array
  * @size: array size
