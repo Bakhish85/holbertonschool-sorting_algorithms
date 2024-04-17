a59103ebd6ef96729f30b539c7f6d7152869c9fa
Bakhish85 <umudlu85@gmail.com>
2024-04-17 09:13:58 +0000

    quick sort

--- swap.c
+++ swap.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "sort.h"
 /**
- * swap - Function that swaps two values
+ * _swap - Function that swaps two values
  * @a: Fisrt value
  * @b: Second value
  * Return: 0
  */
-void swap(int *a, int *b)
+void _swap(int *a, int *b)
 {
 	int tmp;
 
